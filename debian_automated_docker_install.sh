eb34135a4af17dff7f0c61ca2dbf4e6af4a7ea36
codeserver <thatg33k@pm.me>
2024-01-24 22:49:23 +0100

    updated compose version

--- debian_automated_docker_install.sh
+++ debian_automated_docker_install.sh
@@ -21,7 +21,7 @@ echo "install docker"
 sudo apt update && apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 echo "also install docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/2.24.3/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
