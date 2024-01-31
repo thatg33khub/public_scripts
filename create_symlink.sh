f3717014b3819a014c2b3c8dfaeb778f555f4210
thatg33k <thatg33k@pm.me>
2024-01-31 22:25:04 +0100

    updated descr

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# The script must be located in the project folder, above the stack folders:
+# /opt/stacks/create_symlink.sh
+# /opt/stacks/bitwarden
+# /opt/stacks/portainer
+# ...
+
 # Define location of source .env file
 ENVDIR=/some/path
 
