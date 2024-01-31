e85a238d669e573d2e3f9c13dcb78bdf1b441756
thatg33k <thatg33k@pm.me>
2024-01-31 22:32:08 +0100

    updated path + instructions

--- start-stop-restart_all_containers.sh
+++ start-stop-restart_all_containers.sh
@@ -1,14 +1,16 @@
+# The script must be located in the project folder, above the stack folders:
+# /opt/stacks/create_symlink.sh
+# /opt/stacks/bitwarden
+# /opt/stacks/portainer
+# ...
+
+
 # Check that the script runs with sudo
 if [ "$USER" != "root" ]
 then
     echo "Please run this as root or with sudo"
     exit 2
 fi
-
-# Assuming the following file structure: /home/user/docker/application_name/docker-compose.yml
-echo "move to the docker folder"
-echo "move to /home/${SUDO_USER}/docker/"
-cd /home/${SUDO_USER}/docker/
 
 # Choose the command to execute
 echo "Choose the command you want to use:"
@@ -18,18 +20,18 @@ echo "3 - Restart all containers"
 read docker_command;
 
 # Execute the docker compose command
-case $docker_command in 
-    [1] ) echo "ok, starting the containers" ; 
+case $docker_command in
+    [1] ) echo "ok, starting the containers" ;
         for dir in */; do
         docker compose -f $dir/docker-compose.yml up -d
         done;
         break;;
-    [2] ) echo "ok, stopping the containers" ; 
+    [2] ) echo "ok, stopping the containers" ;
         for dir in */; do
         docker compose -f $dir/docker-compose.yml down
         done;
         break;;
-    [3] ) echo "ok, restarting the containers" ; 
+    [3] ) echo "ok, restarting the containers" ;
         for dir in */; do
         docker compose -f $dir/docker-compose.yml restart
         done;
